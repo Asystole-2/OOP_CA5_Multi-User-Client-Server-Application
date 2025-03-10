4aa7880a51149403a82480a64297117cbc83270e
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-03-10 22:55:42 +0000

    json converter class + methods; tested in menu and working. will make the display nicer

--- src/main/java/DTOs/JsonConverter.java
+++ src/main/java/DTOs/JsonConverter.java
@@ -10,8 +10,30 @@ import java.util.ArrayList;
 
 public class JsonConverter {
 
-    public void MoviesConverter(ArrayList<Movie> movies) throws DaoException {
+    public void MoviesConverter(List<Movie> movies) throws DaoException {
 
         JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray(movies);
+        jsonObject.put("movies", jsonArray);
+
+        String jsonString = jsonObject.toString();
+        System.out.println("\nJson String for the Movie entity list:\n" + jsonString);
+    }
+    public void BookingConverter(List<Booking> bookings) throws DaoException {
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray(bookings);
+        jsonObject.put("bookings", jsonArray);
+
+        String jsonString = jsonObject.toString();
+        System.out.println("\nJson String for the Booking entity list\n:" + jsonString);
+    }
+    public void CustomerConverter(List<Customer> customers) throws DaoException {
+        JSONObject jsonObject = new JSONObject();
+        JSONArray jsonArray = new JSONArray(customers);
+        jsonObject.put("customers", jsonArray);
+
+        String jsonString = jsonObject.toString();
+        System.out.println("\nJson String for the Customer entity list\n:" + jsonString);
+
     }
 }
