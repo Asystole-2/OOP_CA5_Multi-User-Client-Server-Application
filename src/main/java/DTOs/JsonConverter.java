68d1ddd863ff353deeea2372f18de9e47060c284
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-03-10 22:27:51 +0000

    start

---
+++ src/main/java/DTOs/JsonConverter.java
@@ -0,0 +1,17 @@
+package DTOs;
+
+import Exceptions.DaoException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
+
+public class JsonConverter {
+
+    public void MoviesConverter(ArrayList<Movie> movies) throws DaoException {
+
+        JSONObject jsonObject = new JSONObject();
+    }
+}
