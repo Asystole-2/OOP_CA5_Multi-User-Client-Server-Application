2b5d39a08b4da1eceb2c8f86cdaf99a88507a8e3
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-03-14 19:54:49 +0000

    customer dao j unit test

---
+++ src/test/java/DAOs/CustomerDaoTest.java
@@ -0,0 +1,33 @@
+package DAOs;
+
+import DTOs.Customer;
+import Exceptions.DaoException;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CustomerDaoTest {
+    CustomerDaoInterface ICustomerDao = null;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() throws DaoException {
+        ICustomerDao = new CustomerDAO();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllCustomers() throws DaoException {
+        List<Customer> Customers = ICustomerDao.getAllCustomers();
+        assertNotNull(Customers);
+
+        assertNotNull(Customers, "The list of customers should not be null.");
+        assertFalse(Customers.isEmpty(), "The list of customers should not be empty.");
+        assertEquals(20, Customers.size(), "Expected 20 customers in the list.");
+
+        Customer firstCustomer = Customers.get(0);
+        assertEquals(1, firstCustomer.getCustomer_id(), "The ID of the first customer should be 1.");
+        assertEquals("user1", firstCustomer.getUserName(), "The name of the first customer should be user1.");
+    }
+}
